0821d21ad8617240be07aaba57f4f8313e0e08a7
sheena.griffin@gmail.com <sheena.griffin@gmail.com>
2018-09-03 20:46:20 +0000

    adding powers & modulo

--- calc.py
+++ calc.py
@@ -10,6 +10,8 @@ Please type in the math operation you would like to complete:
 - for subtraction
 * for multiplication
 / for division
+** for power
+% for modulo
 ''')
 
 	number_1 = int(input('Enter your first number: '))
@@ -34,6 +36,23 @@ Please type in the math operation you would like to complete:
 	elif operation == '/':
 		print('{} / {} = '.format(number_1, number_2))
 		print(number_1 / number_2)
+
+# Power
+	elif operation == '**':
+		powerTotal = 0
+		count = number_2
+		while (count >= 0):
+    			print(count)
+			newPower = number_1*number_1
+			count = count - 1
+			powerTotal = powerTotal + newPower
+		print('{} ^ {} = '.format(number_1, number_2))
+		print(powerTotal)
+
+# Modulo
+	elif operation == '%':
+		print('{} % {} = '.format(number_1, number_2))
+		print(number_1 % number_2)
 
 	else:
 	    	print('You have not typed a valid operator, please run the program again.')
